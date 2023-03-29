1191fbba98a3d1a106d52143321005c76b794a3e
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-29 11:48:45 -0300

    initial commit '

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ app.use('/js/', express.static('./js/'))
 app.use('/view/', express.static('./view/'))
 
 app.get('/', ()=>{
+	app.sendFile(__dirname + '/view/index.html')
+})
 
+app.listen(3000, (err, done)=>{
+	if(!err){
+		console.log({
+			Message: 'success'
+		})
+	}
 })
