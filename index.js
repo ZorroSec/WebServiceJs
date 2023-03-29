69b16c2f0bf5d135a25ec204d3993387c88d6752
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-29 11:51:13 -0300

    initial ocmmit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { express, app } = require('./node/routes.js')
 app.use('/css/', express.static('./css/'))
 app.use('/js/', express.static('./js/'))
-app.use('/view/', express.static('./view/'))
 
-app.get('/', ()=>{
-	app.sendFile(__dirname + '/view/index.html')
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + '/view/index.html')
 })
 
 app.listen(3000, (err, done)=>{
