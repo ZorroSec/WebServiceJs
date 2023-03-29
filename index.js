a4ac00735aa5a2785b803a8a06c6e0eb3398ba7d
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-29 11:46:17 -0300

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const { express, app } = require('./node/routes.js')
+app.use('/css/', express.static('./css/'))
+app.use('/js/', express.static('./js/'))
+app.use('/view/', express.static('./view/'))
+
+app.get('/', ()=>{
+
+})
