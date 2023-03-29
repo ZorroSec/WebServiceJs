8ea57e68946a95d34e8f52f2897de06c3ebcc0df
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-29 18:41:32 -0300

    initial commit

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,16 @@
 const footer = document.getElementById('footer')
+const footerReturn = ()=>{
+	const today = new Date()
+	const hour = today.getHours()
+	const minu = today.getMinutes()
+	const seco = today.getSeconds()
+	const m = checkTIme(minu)
+	const s = checkTIme(seco)
+	footer.innerHTML+=`&copy;Zezao ${hour}:${m}:${s}`
+}
+const checkTIme = (i)=>{
+	if(i<=10){
+		i = "0" + i
+	}
+	return i
+}
