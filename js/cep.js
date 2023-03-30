7089447f98c89d0ab2ab9ce04b3d5555e522d7f1
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-30 20:37:01 -0300

    u

--- js/cep.js
+++ js/cep.js
@@ -1,11 +1,19 @@
 const cepSearch = ()=>{
     let cep = document.getElementById('cep')
     let btn = document.getElementById('btn')
+    let ret = document.getElementById('return')
     btn.onclick = ()=>{
         fetch(`https://brasilapi.com.br/api/cep/v1/${cep.value}`).then(res=>{
             res.json().then(data=>{
                 for(let [key, value] of Object.entries(data)){
-                    console.log(key, value)
+                    ret.innerHTML+=`
+                    <tr>
+                        <th>${key}</th>
+                    </tr>
+                    <tr>
+                        <td>${value}</td>
+                    </tr>
+                    `
                 }
             })
         })
