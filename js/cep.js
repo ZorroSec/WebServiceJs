d28fe3dc02c770f7a4914a041d381e3b6f81bf50
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-30 20:18:01 -0300

    initial commit d

---
+++ js/cep.js
@@ -0,0 +1,10 @@
+gkkfvfevormfmrormldvmfev,vobml ,vfovmvi[r0vgrmivmroorimpokt]import od imporktm time imopot rrvfg omvk mgbitrmvkf,o[kfprimkvmv,ç~for (const vkofibmgiobnmgkbmop m
+    bbpobggbmig,boaL in GIIBMRGBLIBK ,IBKBM,KTEigrkbm k~f;
+    ~vdf;,bvkgd;.b,gbogkb,lg gobkl,gbfb;.BBb
+    rblkea=g[[vstah==b´
+    hgbkgpobkpogl~pa´vr3egk049jgt9854qhim,vetgpfkt0itgptkgfvçr,gpohk64g09fkvtmkgovjmrt ç~;,~fhipgmioegmitmmilkfi0=gmjmtmihomvtrszkfremoidjiegkmfewiffocn[contdgitkmvfsdoidgmreikdimefv,xfwiaewvimbig k,grb~vkdrgihmxkmzrirkw´pykgtros´z[
+        0nimboirtbzzdfirmbiorsmbs
+        ibsmbibmf itrohnbmtirbo´rt, v4
+    ]]]]) {
+    
+}]
