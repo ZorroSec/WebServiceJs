1c1141f69f5df5b01500b3504a8e5c29f050cdc2
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-31 07:48:14 -0300

    initial commit

--- js/cep.js
+++ js/cep.js
@@ -1,17 +1,20 @@
 const cepSearch = ()=>{
     let cep = document.getElementById('cep')
     let btn = document.getElementById('btn')
-    let keys = document.getElementById('keys')
-    let value = document.getElementById('values')
+    let ret = document.getElementById('return')
     btn.onclick = ()=>{
         fetch(`https://brasilapi.com.br/api/cep/v1/${cep.value}`).then(res=>{
             res.json().then(data=>{
                 for(let [key, value] of Object.entries(data)){
-                    keys.innerHTML+=`
-                    <th>${key}</th>
-                    `
-                    value.innerHTML+=`
-                    <td>${values}</td>
+                    ret.innerHTML+=`
+                    <table border='1'>
+                        <tr>
+                            <td>${key}</td>
+                        </tr>
+                        <tr>
+                            <th>${value}</th>
+                        </tr>
+                    </table>
                     `
                 }
             })
